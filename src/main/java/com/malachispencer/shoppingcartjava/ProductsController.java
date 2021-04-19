f3743e60661311cb657bfefdf1962b27f8c13fb2
Malachi Spencer <m.spencer@live.co.uk>
2021-04-19 20:48:27 +0100

    Displaying database products on view

---
+++ src/main/java/com/malachispencer/shoppingcartjava/ProductsController.java
@@ -0,0 +1,26 @@
+package com.malachispencer.shoppingcartjava;
+
+import com.malachispencer.shoppingcartjava.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductsController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/")
+    public String products(Model model) {
+        model.addAttribute(
+            "products",
+            productRepository.findAll(
+                Sort.by(Sort.Direction.ASC, "productID")
+            )
+        );
+
+        return "products";
+    }
+}
