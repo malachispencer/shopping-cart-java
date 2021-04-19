eb81bbc03a834a842248de6bf5f3b5d630adce37
Malachi Spencer <m.spencer@live.co.uk>
2021-04-19 18:26:26 +0100

    Initial commit

---
+++ src/main/java/com/malachispencer/shoppingcartjava/ShoppingCartJavaApplication.java
@@ -0,0 +1,13 @@
+package com.malachispencer.shoppingcartjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malachispencer/shoppingcartjava/ShoppingCartJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malachispencer.shoppingcartjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
