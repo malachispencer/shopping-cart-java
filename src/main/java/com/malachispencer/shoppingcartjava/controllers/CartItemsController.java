fb16f4570a16873bc7ce02affe6dde5002066f66
Malachi Spencer <m.spencer@live.co.uk>
2021-04-22 10:56:51 +0100

    Receiving form data in controller, ready to pass onto custom repository impl

--- src/main/java/com/malachispencer/shoppingcartjava/controllers/CartItemsController.java
+++ src/main/java/com/malachispencer/shoppingcartjava/controllers/CartItemsController.java
@@ -4,16 +4,19 @@ import com.malachispencer.shoppingcartjava.models.CartItem;
 import com.malachispencer.shoppingcartjava.models.Product;
 import com.malachispencer.shoppingcartjava.repositories.CartItemRepository;
 import com.malachispencer.shoppingcartjava.repositories.ProductRepository;
+import jdk.jfr.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,9 +32,10 @@ public class CartItemsController {
     public String index(Model model) {
         Long itemsInCart = cartItemRepository.count();
         List<Map> cart = cartItemRepository.getItems();
-
         BigDecimal cartTotal = cartItemRepository.cartTotal();
+        Map<String, Integer> map = new HashMap();
 
+        model.addAttribute("map", map);
         model.addAttribute("cartItems", cart);
         model.addAttribute("cartTotal", cartTotal);
         model.addAttribute("itemsInCart", itemsInCart);
@@ -62,5 +66,17 @@ public class CartItemsController {
         productRepository.decrementStock(newInStock, productID);
 
         return "redirect:/";
+    }
+
+    @PatchMapping(
+        value = "/cart/{id}",
+        consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE}
+    )
+    public String update(@PathVariable("id") String cartID, @RequestParam Map cartData) {
+        cartData.remove("_method");
+        cartData.put("cartID", cartID);
+        System.out.println(cartData);
+
+        return "redirect:/cart";
     }
 }
